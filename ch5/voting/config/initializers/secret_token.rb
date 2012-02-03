160ff5a65b5b5bad3148aab18a92aaf4d814f9cb
Dave Doolin <david.doolin@gmail.com>
2012-02-03 12:11:59 -0800

    started voting application

---
+++ ch5/voting/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Voting::Application.config.secret_token = 'f9468688fedbde53384288450f7d9387a69c2c4c48c85342c1352ff42e00078c247daf5b7dc4af562a093d1ac70f879bf749a6edab7830b7fb2982f1fa7c621e'
