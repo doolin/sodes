eed56252812dd75dd56573c15c871bbadb55bddf
Dave Doolin <david.doolin@gmail.com>
2012-02-03 22:17:32 -0800

    finished first part of chapter 1 of soa

--- ch1/user-service/service.rb
+++ ch1/user-service/service.rb
@@ -32,3 +32,46 @@ post '/api/v1/users' do
       error 400, e.message.to_json
   end
 end
+
+put '/api/v1/users/:name' do
+  user = User.find_by_name(params[:name])
+  if user
+    begin
+      if user.update_attributes(JSON.parse(request.body.read))
+        user.to_json
+      else
+        error 400, user.errors.to_json
+      end
+    rescue => e
+      error 400, e.message.to_json
+    end
+  else
+    error 404, { :error => "user #{params[:name]} not found" }.to_json
+  end
+end
+
+delete '/api/v1/users/:name' do
+  user = User.find_by_name(params[:name])
+  if user
+    user.destroy
+    user.to_json
+  else
+    error 404, { :error => "user #{params[:name]} not found" }.to_json
+  end
+end
+
+post '/api/v1/users/:name/sessions' do
+  begin
+    attributes = JSON.parse(request.body.read)
+    puts attributes
+    user = User.find_by_name_and_password(params[:name],attributes["password"])
+    p "USER: ", user
+    if user
+      user.to_json
+    else
+      error 400, { :error => "invalid login credentials" }.to_json
+    end
+  rescue => e
+    error 400, e.message.to_json
+  end
+end
