fac33cb886c8a987fdab121f149dca1fbf7bb8b6
Dave Doolin <david.doolin@gmail.com>
2012-02-22 09:16:41 -0800

    more work on ruby soa

--- ch3/socialfeed/app/models/comment.rb
+++ ch3/socialfeed/app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :entry, :counter_cache => true
+  # Replication? Page 64 of SOAR
   after_create { |record| CommentActivity.write(record) }
 end
